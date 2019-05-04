02ceb5822b3b4f5e1c95c2eb65a9f9acbeb3920f
LiZhuBin <LiZhuBin@github.com>
2019-05-04 17:29:47 +0800

    rebuilding site 2019年05月 4日 17:29:44

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,24 @@
-#!/bin/sh
+#! /bin/bash
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
+set -eux
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+msg="rebuilding site `date`"
+
+if [ $# -eq 1  ]
+    then msg="$1"
 fi
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-rm -rf .git/worktrees/public/
+# Build the project.
+hugo -d docs
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+# Add changes to git.
+git add .
 
-echo "Removing existing files"
-rm -rf public/*
+# Commit changes.
 
-echo "Generating site"
-hugo
+git commit -m "$msg"
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
-
-echo "Push to origin"
-git push origin gh-pages
+# Push source and build repos.
+git push origin master
